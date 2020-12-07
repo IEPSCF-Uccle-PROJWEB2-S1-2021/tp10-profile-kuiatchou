85a7e496af7b8a55b397d05e71d4beb3e4ec03cf
kuiatchou merveille <kume0804@student.iepscf-uccle.be>
2020-12-07 20:31:44 +0100

    init

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = new express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'formulaire' });
 });
 
 module.exports = router;
